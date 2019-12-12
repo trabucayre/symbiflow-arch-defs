44f3c133c900468ec1c5efc64bbb5d2435d8e789
Tomasz Michalak <tmichalak@antmicro.com>
2019-12-12 12:52:23 +0100

    Add oserdes to obuftds test

--- xc7/tests/obuftds/obuftds_basys3.v
+++ xc7/tests/obuftds/obuftds_basys3.v
@@ -24,46 +24,103 @@ a separate bank.
 `default_nettype none
 
 // ============================================================================
+`define CLKFBOUT_MULT 2
 
 module top
 (
-input  wire [11:8] sw,
+input wire clk,
+input wire rst,
 
-output wire [1:0]  diff_p,
-output wire [1:0]  diff_n
+input  wire [7:0] sw,
+
+output wire  diff_p,
+output wire  diff_n
+);
+wire [7:0] INPUTS;
+wire  buf_i;
+wire  buf_t;
+wire CLK;
+wire CLKDIV;
+
+BUFG bufg(.I(clk), .O(CLK));
+
+assign INPUTS = sw;
+
+
+OSERDESE2 #(
+.DATA_RATE_OQ   ("DDR"),
+.DATA_WIDTH     (8),
+.DATA_RATE_TQ   ("BUF"),
+.TRISTATE_WIDTH (1)
+)
+oserdes
+(
+.CLK    (CLK),
+.CLKDIV (CLKDIV),
+.RST    (rst),
+
+.OCE    (1'b1),
+.D1     (INPUTS[0]),
+.D2     (INPUTS[1]),
+.D3     (INPUTS[2]),
+.D4     (INPUTS[3]),
+.D5     (INPUTS[4]),
+.D6     (INPUTS[5]),
+.D7     (INPUTS[6]),
+.D8     (INPUTS[7]),
+.OQ     (buf_i),
+
+.TCE    (1'b1),
+.T1     (1'b0), // All 0 to keep OBUFT always on.
+.T2     (1'b0),
+.T3     (1'b0),
+.T4     (1'b0),
+.TQ     (buf_t)
 );
 
 // ============================================================================
 // OBUFTDS
-wire [1:0] buf_i;
-wire [1:0] buf_t;
 
 OBUFTDS # (
   .IOSTANDARD("DIFF_SSTL135"),
   .SLEW("FAST")
 ) obuftds_0 (
-  .I(buf_i[0]),
-  .T(buf_t[0]),
-  .O(diff_p[0]), // LED2
-  .OB(diff_n[0]) // LED3
+  .I(buf_i),
+  .T(buf_t),
+  .O(diff_p), // LED2
+  .OB(diff_n) // LED3
 );
 
-OBUFTDS # (
-  .IOSTANDARD("DIFF_SSTL135"),
-  .SLEW("FAST")
-) obuftds_1 (
-  .I(buf_i[1]),
-  .T(buf_t[1]),
-  .O(diff_p[1]), // LED8
-  .OB(diff_n[1]) // LED7
+wire locked;
+wire pre_bufg_clkdiv;
+wire clk_fb_i;
+wire clk_fb_o;
+
+PLLE2_ADV #(
+.BANDWIDTH          ("HIGH"),
+.COMPENSATION       ("ZHOLD"),
+.CLKIN1_PERIOD      (10.0),  // 100MHz
+.CLKFBOUT_MULT      (`CLKFBOUT_MULT),
+.CLKOUT0_DIVIDE     (`CLKFBOUT_MULT * 4),
+.CLKOUT1_DIVIDE     ((`CLKFBOUT_MULT * 4) * 4),
+.STARTUP_WAIT       ("FALSE"),
+.DIVCLK_DIVIDE      (1'd1)
+)
+pll
+(
+.CLKIN1     (CLK),
+.CLKINSEL   (1),
+
+.RST        (rst),
+.PWRDWN     (0),
+.LOCKED     (locked),
+
+.CLKFBIN    (clk_fb_i),
+.CLKFBOUT   (clk_fb_o),
+
+.CLKOUT0    (pre_bufg_clkdiv),
 );
 
-// ============================================================================
-
-assign buf_i[0] = sw[ 8];
-assign buf_t[0] = sw[ 9];
-assign buf_i[1] = sw[10];
-assign buf_t[1] = sw[11];
+BUFG bufg_clkdiv(.I(pre_bufg_clkdiv), .O(CLKDIV));
 
 endmodule
-
