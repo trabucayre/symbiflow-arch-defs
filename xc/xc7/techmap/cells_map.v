97a33e9ab729fbb48c449d64ecf15ae2bf080989
Robert Winkler <rwinkler@antmicro.com>
2020-04-14 13:00:17 +0200

    Add BUFGCE and BUFMUX

--- xc/xc7/techmap/cells_map.v
+++ xc/xc7/techmap/cells_map.v
@@ -2941,6 +2941,46 @@ module BUFG (
   );
 endmodule
 
+module BUFGCE (
+  input I,
+  input CE,
+  output O,
+  );
+
+  BUFGCTRL _TECHMAP_REPLACE_ (
+    .O(O),
+    .CE0(CE),
+    .CE1(1'b0),
+    .I0(I),
+    .I1(1'b1),
+    .IGNORE0(1'b0),
+    .IGNORE1(1'b1),
+    .S0(1'b1),
+    .S1(1'b0)
+  );
+endmodule
+
+module BUFGMUX (
+  input I0,
+  input I1,
+  input S,
+  output O
+  );
+
+  BUFGCTRL _TECHMAP_REPLACE_ (
+    .O(O),
+    .CE0(!S),
+    .CE1(S),
+    .I0(I0),
+    .I1(I1),
+    .IGNORE0(1'b0),
+    .IGNORE1(1'b0),
+    .S0(1'b1),
+    .S1(1'b1)
+  );
+endmodule
+
+
 module BUFGCTRL (
 output O,
 input I0, input I1,
