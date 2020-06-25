fa4b2c529bc45d69abe987baeb3557cc3c4157ff
rakeshm75 <54097448+rakeshm75@users.noreply.github.com>
2020-06-26 01:36:20 +0530

    added fpga_interconnect.sim file

--- quicklogic/primitives/fpga_interconnect/fpga_interconnect.sim.v
+++ quicklogic/primitives/fpga_interconnect/fpga_interconnect.sim.v
@@ -1,1 +1,14 @@
+`timescale 1ns/10ps
+(* whitebox *)
+module fpga_interconnect(
+    input datain,
+    output dataout
+);
 
+    specify
+        (datain=>dataout)="";
+    endspecify
+
+    assign dataout = datain;
+
+endmodule
