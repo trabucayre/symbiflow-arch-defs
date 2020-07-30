2eeb3cb88e9938bb84f95ed7a619bb668a9525d7
kkumar23 <61860672+kkumar23@users.noreply.github.com>
2020-07-30 22:18:22 +0530

    revert to make from conda

--- .github/travis/script.sh
+++ .github/travis/script.sh
@@ -9,21 +9,14 @@ $SPACER
 #setup install path
 INSTALL_DIR="$(pwd)/install"
 
-#setup ninja
-export CMAKE_FLAGS="-GNinja -DCMAKE_INSTALL_PREFIX=${INSTALL_DIR}"
-export BUILD_TOOL=ninja
-
-$SPACER
-
 start_section "symbiflow.configure_cmake" "Configuring CMake (make env)"
 make env
 cd build
 end_section "symbiflow.configure_cmake"
 
 $SPACER
+make_target all_conda "Setting up basic ${YELLOW}conda environment${NC}"
 
-ninja -j$(nproc) all_conda
-#make_target all_conda "Setting up basic ${YELLOW}conda environment${NC}"
 cp -r env/conda/share/yosys/* env/conda/share
 
 $SPACER
@@ -40,13 +33,5 @@ $SPACER
 
 echo "Running quicklogic testsuit"
 
-#cd quicklogic/pp3/tests
-echo "----------------------------------------"
-(
-#	pushd build
-	export VPR_NUM_WORKERS=${nproc}
-	ninja -j$(nproc) all_quick_tests
-#	popd
-)
-echo "----------------------------------------"
-#make_target all_quick_tests -j10
+cd quicklogic/pp3/tests
+make_target all_quick_tests -j10
