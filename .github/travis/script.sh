e7e9afee58671689a5c28d543a0d68d48618a2d9
kkumar23 <61860672+kkumar23@users.noreply.github.com>
2020-10-23 17:06:32 +0530

    Update script.sh

--- .github/travis/script.sh
+++ .github/travis/script.sh
@@ -15,10 +15,6 @@ $SPACER
 make_target all_conda "Setting up basic ${YELLOW}conda environment${NC}"
 
 $SPACER
-
-cp -r env/conda/share/yosys/* env/conda/share
-
-$SPACER
 # Output some useful info
 start_section "info.conda.env" "Info on ${YELLOW}conda environment${NC}"
 env/conda/bin/conda info
