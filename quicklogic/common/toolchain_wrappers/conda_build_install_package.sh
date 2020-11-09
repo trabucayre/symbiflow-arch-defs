3584b78218731405cebcd9005b10166a7f83e3e4
kkumar23 <61860672+kkumar23@users.noreply.github.com>
2020-11-09 10:56:22 +0530

    Update conda_build_install_package.sh
    Fix For:
    Mangling user configuration without asking (especially the channel list and the always_yes setting, but disabling changeps1 setting as well) is user-hostile. Using system/user channels in the Symbiflow installer means the packages that get actually installed are potentially broken and definitely nonreproducible.

--- quicklogic/common/toolchain_wrappers/conda_build_install_package.sh
+++ quicklogic/common/toolchain_wrappers/conda_build_install_package.sh
@@ -19,17 +19,15 @@ mkdir -p $INSTALL_DIR
 wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O conda_installer.sh
 bash conda_installer.sh -b -p $INSTALL_DIR/conda && rm conda_installer.sh
 source "$INSTALL_DIR/conda/etc/profile.d/conda.sh"
-conda config --set always_yes yes --set changeps1 no
-conda config --add channels conda-forge
-conda config --add channels quicklogic-corp/label/ql
-conda update -y -q conda
+CONDA_FLAGS="-y --override-channels -c defaults -c conda-forge"
+conda update $CONDA_FLAGS -q conda
 wget -qO- https://quicklogic-my.sharepoint.com/:u:/p/kkumar/EQro5PGhiMpBhlieiFNoiuwBdQ-lpi-3VjqvRmYow54hEA?download=1 | tar -xJ -C $INSTALL_DIR
-conda install -c quicklogic-corp/label/ql yosys="0.8.0_0002_gc3b38fdc 20200901_073908" python=3.7
-conda install -c quicklogic-corp/label/ql yosys-plugins="1.2.0_0009_g9ab211c 20201001_121833"
-conda install -c quicklogic-corp/label/ql vtr="v8.0.0_rc2_2894_gdadca7ecf 20201008_140004"
-conda install -c quicklogic-corp iverilog
-conda install -c tfors gtkwave
-conda install -y make lxml simplejson intervaltree git pip
+conda install $CONDA_FLAGS -c quicklogic-corp/label/ql yosys="0.8.0_0002_gc3b38fdc 20200901_073908" python=3.7
+conda install $CONDA_FLAGS -c quicklogic-corp/label/ql yosys-plugins="1.2.0_0009_g9ab211c 20201001_121833"
+conda install $CONDA_FLAGS -c quicklogic-corp/label/ql vtr="v8.0.0_rc2_2894_gdadca7ecf 20201008_140004"
+conda install $CONDA_FLAGS -c quicklogic-corp iverilog
+conda install $CONDA_FLAGS -c tfors gtkwave
+conda install $CONDA_FLAGS make lxml simplejson intervaltree git pip
 conda activate
 pip install python-constraint
 pip install serial
