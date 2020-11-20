14a2ef37b7fa89dfd8840d0438c07b0079f39cc8
kkumar23 <61860672+kkumar23@users.noreply.github.com>
2020-11-20 18:55:45 +0530

    Update conda_build_install_package.sh
    adding env setting and build from storage cloud

--- quicklogic/common/toolchain_wrappers/conda_build_install_package.sh
+++ quicklogic/common/toolchain_wrappers/conda_build_install_package.sh
@@ -19,9 +19,11 @@ mkdir -p $INSTALL_DIR
 wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O conda_installer.sh
 bash conda_installer.sh -b -p $INSTALL_DIR/conda && rm conda_installer.sh
 source "$INSTALL_DIR/conda/etc/profile.d/conda.sh"
+echo "include-system-site-packages=false" >> $INSTALL_DIR/conda/pyvenv.cfg
 CONDA_FLAGS="-y --override-channels -c defaults -c conda-forge"
 conda update $CONDA_FLAGS -q conda
-wget -qO- https://quicklogic-my.sharepoint.com/:u:/p/kkumar/EQro5PGhiMpBhlieiFNoiuwBdQ-lpi-3VjqvRmYow54hEA?download=1 | tar -xJ -C $INSTALL_DIR
+curl https://storage.googleapis.com/symbiflow-arch-defs-install/quicklogic-arch-defs-63c3d8f9.tar.gz --output arch.tar.gz
+tar -C $INSTALL_DIR -xvf arch.tar.gz && rm arch.tar.gz
 conda install $CONDA_FLAGS -c quicklogic-corp/label/ql yosys="0.8.0_0002_gc3b38fdc 20200901_073908" python=3.7
 conda install $CONDA_FLAGS -c quicklogic-corp/label/ql yosys-plugins="1.2.0_0009_g9ab211c 20201001_121833"
 conda install $CONDA_FLAGS -c quicklogic-corp/label/ql vtr="v8.0.0_rc2_2894_gdadca7ecf 20201008_140004"
@@ -31,5 +33,5 @@ conda install $CONDA_FLAGS make lxml simplejson intervaltree git pip
 conda activate
 pip install python-constraint
 pip install serial
-pip install git+https://github.com/QuickLogic-Corp/quicklogic-fasm
+pip install git+https://github.com/QuickLogic-Corp/quicklogic-fasm@318abca
 conda deactivate
