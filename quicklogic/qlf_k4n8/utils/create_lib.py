430b528971498a99e9b815c322ae9a6e4e3524f5
Lalit Narain Sharma <61820431+lnsharma@users.noreply.github.com>
2021-04-29 16:56:21 +0530

    Grouping F2A and A2F pins in a bus in the generated lib file.

--- quicklogic/qlf_k4n8/utils/create_lib.py
+++ quicklogic/qlf_k4n8/utils/create_lib.py
@@ -6,7 +6,9 @@ import argparse
 import sys
 import csv
 import os
+import re
 from collections import namedtuple
+from collections import defaultdict
 from datetime import date
 import simplejson as json
 import lxml.etree as ET
@@ -101,7 +103,7 @@ def main():
         )
         sys.exit(1)
 
-    csv_pin_data = set()
+    csv_pin_data = defaultdict(set)
     assoc_clk = dict()
     with open(args.csv, newline='') as csvfile:
         reader = csv.DictReader(csvfile)
@@ -109,7 +111,10 @@ def main():
             # define properties for scalar pins
             scalar_port_names = vec_to_scalar(row['port_name'])
             for port in scalar_port_names:
-                csv_pin_data.add(port)
+                if port.find('F2A') != -1:
+                    csv_pin_data['F2A'].add(port)
+                elif port.find('A2F') != -1:
+                    csv_pin_data['A2F'].add(port)
                 if row['Associated Clock'] is not None:
                     assoc_clk[port] = row['Associated Clock'].strip()
 
@@ -147,14 +152,12 @@ def create_lib(
     str_rep_date = curr_str.replace("{curr_date}", curr_date)
     str1 = str_rep_date.replace("{cell_name}", cell_name)
 
-    pins = []
-    for port in port_names:
+    a2f_pin_list = [None]*len(port_names['A2F'])
+    f2a_pin_list = [None]*len(port_names['F2A'])
+    for port in port_names['A2F']:
         pin_dir = "input"
-        if port.find("F2A") != -1:
-            pin_dir = "output"
-
         pin_used = False
-        if port in csv_pin_data:
+        if port in csv_pin_data['A2F']:
             pin_used = True
 
         clk = ""
@@ -162,63 +165,110 @@ def create_lib(
             if assoc_clk[port].strip() != '':
                 clk = assoc_clk[port].strip()
 
-        pin_data = PinData(name=port, dir=pin_dir, used=pin_used, clk=clk)
-        pins.append(pin_data)
+        index = port_index(port)
+        if index != -1:
+            pin_data = PinData(name=port, dir=pin_dir, used=pin_used, clk=clk)
+            a2f_pin_list[index] = pin_data
+        else:
+            print("ERROR: No index present in A2F port: '{}'".format(port))
+
+    for port in port_names['F2A']:
+        pin_dir = "output"
+        pin_used = False
+        if port in csv_pin_data['F2A']:
+            pin_used = True
+
+        clk = ""
+        if port in assoc_clk:
+            if assoc_clk[port].strip() != '':
+                clk = assoc_clk[port].strip()
+
+        index = port_index(port)
+        if index != -1:
+            pin_data = PinData(name=port, dir=pin_dir, used=pin_used, clk=clk)
+            f2a_pin_list[index] = pin_data
+        else:
+            print("ERROR: No index present in F2A port: '{}'\n".format(port))
+
 
     lib_data = ""
-    for pin in pins:
+    a2f_bus_name = ""
+    if len(a2f_pin_list) > 0:
+        pos = a2f_pin_list[0].name.find('[')
+        if pos != -1:
+            a2f_bus_name = a2f_pin_list[0].name[0:pos]
+            lib_data += "\n{}bus ( {} ) {{\n".format(add_tab(2), a2f_bus_name)
+            lib_data += "\n{}bus_type   : BUS1536_type1 ;".format(add_tab(3))
+            lib_data += "\n{}direction  : input ;\n".format(add_tab(3))
+
+    for pin in a2f_pin_list:
         if pin.used:
-            if pin.dir == "input":
-                curr_str = "\n\t\tpin ({}) {{".format(pin.name)
-                cap = common_lib_data["input_used"]['cap']
-                max_tran = common_lib_data["input_used"]['max_tran']
-                curr_str += form_pin_header(pin.dir, cap, max_tran)
+            curr_str = "\n{}pin ({}) {{".format(add_tab(3), pin.name)
+            cap = common_lib_data["input_used"]['cap']
+            max_tran = common_lib_data["input_used"]['max_tran']
+            curr_str += form_pin_header(pin.dir, cap, max_tran)
 
-                if pin.clk != '':
-                    clks = pin.clk.split(' ')
-                    for clk in clks:
-                        clk_name = clk
-                        timing_type = ['setup_rising', 'hold_rising']
-                        for val in timing_type:
-                            curr_str += form_in_timing_group(
-                                clk_name, val, common_lib_data
-                            )
-                curr_str += "\n\t\t}"
-                lib_data += curr_str
-            else:
-                curr_str = "\n\t\tpin ({}) {{".format(pin.name)
-                cap = common_lib_data["output_used"]['cap']
-                max_tran = common_lib_data["output_used"]['max_tran']
-                curr_str += form_pin_header(pin.dir, cap, max_tran)
-
-                if pin.clk != '':
-                    clks = pin.clk.split(' ')
-                    for clk in clks:
-                        clk_name = clk
-                        timing_type = "rising_edge"
-                        curr_str += form_out_timing_group(
-                            clk_name, timing_type, common_lib_data
+            if pin.clk != '':
+                clks = pin.clk.split(' ')
+                for clk in clks:
+                    clk_name = clk
+                    timing_type = ['setup_rising', 'hold_rising']
+                    for val in timing_type:
+                        curr_str += form_in_timing_group(
+                            clk_name, val, common_lib_data
                         )
-                    curr_str += form_out_reset_timing_group(
-                        "RESET_N", "positive_unate", "clear", common_lib_data
-                    )
-                curr_str += "\n\t\t}"
-                lib_data += curr_str
+            curr_str += "\n{}}} /* end of pin {} */\n".format(add_tab(3), pin.name)
+            lib_data += curr_str
         else:
-            if pin.dir == "input":
-                curr_str = "\n\t\tpin ({}) {{".format(pin.name)
-                cap = common_lib_data["input_unused"]['cap']
-                max_tran = common_lib_data["input_unused"]['max_tran']
-                curr_str += form_pin_header(pin.dir, cap, max_tran)
-                curr_str += "\n\t\t}"
-                lib_data += curr_str
-            else:
-                curr_str = "\n\t\tpin ({}) {{".format(pin.name)
-                cap = common_lib_data["output_unused"]['cap']
-                max_tran = common_lib_data["output_unused"]['max_tran']
-                curr_str += form_pin_header(pin.dir, cap, max_tran)
-                curr_str += "\n\t\t}"
-                lib_data += curr_str
+            curr_str = "\n{}pin ({}) {{".format(add_tab(3), pin.name)
+            cap = common_lib_data["input_unused"]['cap']
+            max_tran = common_lib_data["input_unused"]['max_tran']
+            curr_str += form_pin_header(pin.dir, cap, max_tran)
+            curr_str += "\n{}}} /* end of pin {} */\n".format(add_tab(3), pin.name)
+            lib_data += curr_str
+
+    if len(a2f_pin_list) > 0:
+        lib_data += "\n{}}} /* end of bus {} */\n".format(add_tab(2), a2f_bus_name)
+
+    f2a_bus_name = ""
+    if len(f2a_pin_list) > 0:
+        pos = f2a_pin_list[0].name.find('[')
+        if pos != -1:
+            f2a_bus_name = f2a_pin_list[0].name[0:pos]
+            lib_data += "\n{}bus ( {} ) {{\n".format(add_tab(2), f2a_bus_name)
+            lib_data += "\n{}bus_type   : BUS1536_type1 ;".format(add_tab(3))
+            lib_data += "\n{}direction  : input ;\n".format(add_tab(3))
+
+    for pin in f2a_pin_list:
+        if pin.used:
+            curr_str = "\n{}pin ({}) {{".format(add_tab(3), pin.name)
+            cap = common_lib_data["output_used"]['cap']
+            max_tran = common_lib_data["output_used"]['max_tran']
+            curr_str += form_pin_header(pin.dir, cap, max_tran)
+
+            if pin.clk != '':
+                clks = pin.clk.split(' ')
+                for clk in clks:
+                    clk_name = clk
+                    timing_type = "rising_edge"
+                    curr_str += form_out_timing_group(
+                        clk_name, timing_type, common_lib_data
+                    )
+                curr_str += form_out_reset_timing_group(
+                    "RESET_N", "positive_unate", "clear", common_lib_data
+                )
+            curr_str += "\n{}}} /* end of pin {} */\n".format(add_tab(3), pin.name)
+            lib_data += curr_str
+        else:
+            curr_str = "\n{}pin ({}) {{".format(add_tab(3), pin.name)
+            cap = common_lib_data["output_unused"]['cap']
+            max_tran = common_lib_data["output_unused"]['max_tran']
+            curr_str += form_pin_header(pin.dir, cap, max_tran)
+            curr_str += "\n{}}} /* end of pin {} */\n".format(add_tab(3), pin.name)
+            lib_data += curr_str
+    if len(f2a_pin_list) > 0:
+        lib_data += "\n{}}} /* end of bus {} */\n".format(add_tab(2), f2a_bus_name)
+
 
     dedicated_pin_tmpl = template_data_path + "/" + device_name + '_dedicated_pin_lib_data.txt'
     dedicated_pin_lib_file = os.path.join(curr_dir, dedicated_pin_tmpl)
@@ -233,15 +283,26 @@ def create_lib(
 
 # =============================================================================
 
+def port_index(port):
+    """
+    Returns index in the port name like gfpga_pad_IO_A2F[1248]
+    """
+    indx_parser = re.compile(r"[a-zA-Z0-9_]*\[(?P<index>[0-9]+)\]$")
+    match = indx_parser.fullmatch(port)
+    index = -1
+    if match is not None:
+        index = int(match.group("index"))
+
+    return index
 
 def form_pin_header(direction, cap, max_tran):
     '''
     Form pin header section
     '''
-    curr_str = "\n\t\t\tdirection : {};\n\t\t\tcapacitance : {};".format(
-        direction, cap
+    curr_str = "\n{}direction : {};\n{}capacitance : {};".format(
+        add_tab(4), direction, add_tab(4), cap
     )
-    curr_str += "\n\t\t\tmax_transition : {};".format(max_tran)
+    curr_str += "\n{}max_transition : {};".format(add_tab(4), max_tran)
     return curr_str
 
 
@@ -256,20 +317,23 @@ def form_out_reset_timing_group(
     '''
     cell_fall_val = common_lib_data["reset_timing"]['cell_fall_val']
     fall_tran_val = common_lib_data["reset_timing"]['fall_tran_val']
-    curr_str = "\n\t\t\ttiming () {{\n\t\t\t\trelated_pin : \"{}\";".format(
+    curr_str = "\n{}timing () {{\n{}related_pin : \"{}\";".format(
+        add_tab(4),
+        add_tab(5),
         reset_name
     )
-    curr_str += "\n\t\t\t\ttiming_sense : {};".format(timing_sense)
-    curr_str += "\n\t\t\t\ttiming_type : {};".format(timing_type)
-    curr_str += "\n\t\t\t\tcell_fall (scalar) {{\n\t\t\t\t\tvalues({});".format(
-        cell_fall_val
+    curr_str += "\n{}timing_sense : {};".format(add_tab(5), timing_sense)
+    curr_str += "\n{}timing_type : {};".format(add_tab(5), timing_type)
+    curr_str += "\n{}cell_fall (scalar) {{\n{}values({});\n{}}}".format(
+        add_tab(5),
+        add_tab(6),
+        cell_fall_val,
+        add_tab(5)
     )
-    curr_str += "\n\t\t\t\t}"
-    curr_str += "\n\t\t\t\tfall_transition (scalar) {{\n\t\t\t\t\tvalues({});".format(
-        fall_tran_val
+    curr_str += "\n{}fall_transition (scalar) {{\n{}values({});\n{}}}".format(
+        add_tab(5), add_tab(6), fall_tran_val, add_tab(5)
     )
-    curr_str += "\n\t\t\t\t}"
-    curr_str += "\n\t\t\t}"
+    curr_str += "\n{}}}".format(add_tab(4))
     return curr_str
 
 
@@ -288,31 +352,44 @@ def form_out_timing_group(clk_name, timing_type, common_lib_data):
                                     ]['rising_edge_rise_tran_val']
     fall_tran_val = common_lib_data["output_timing"
                                     ]['rising_edge_fall_tran_val']
-    curr_str = "\n\t\t\ttiming () {{\n\t\t\t\trelated_pin : \"{}\";".format(
+    curr_str = "\n{}timing () {{\n{}related_pin : \"{}\";".format(
+        add_tab(4),
+        add_tab(5),
         clk_name
     )
-    curr_str += "\n\t\t\t\ttiming_type : {};".format(timing_type)
-    curr_str += "\n\t\t\t\tcell_rise (scalar) {{\n\t\t\t\t\tvalues({});\n\t\t\t\t}}".format(
-        cell_rise_val
+    curr_str += "\n{}timing_type : {};".format(add_tab(5), timing_type)
+    curr_str += "\n{}cell_rise (scalar) {{\n{}values({});\n{}}}".format(
+        add_tab(5),
+        add_tab(6),
+        cell_rise_val,
+        add_tab(5)
     )
-    curr_str += "\n\t\t\t\trise_transition (scalar) {{\n\t\t\t\t\tvalues({});".format(
-        rise_tran_val
+    curr_str += "\n{}rise_transition (scalar) {{\n{}values({});\n{}}}".format(
+        add_tab(5), add_tab(6), rise_tran_val, add_tab(5)
     )
-    curr_str += "\n\t\t\t\t}"
-    curr_str += "\n\t\t\t\tcell_fall (scalar) {{\n\t\t\t\t\tvalues({});".format(
-        cell_fall_val
+    curr_str += "\n{}cell_fall (scalar) {{\n{}values({});\n{}}}".format(
+        add_tab(5),
+        add_tab(6),
+        cell_fall_val,
+        add_tab(5)
     )
-    curr_str += "\n\t\t\t\t}"
-    curr_str += "\n\t\t\t\tfall_transition (scalar) {{\n\t\t\t\t\tvalues({});".format(
-        fall_tran_val
+    curr_str += "\n{}fall_transition (scalar) {{\n{}values({});\n{}}}".format(
+        add_tab(5), add_tab(6), fall_tran_val, add_tab(5)
     )
-    curr_str += "\n\t\t\t\t}"
-    curr_str += "\n\t\t\t}"
+    curr_str += "\n{}}}".format(add_tab(4))
     return curr_str
 
 
 # =============================================================================
 
+def add_tab(num_tabs):
+    """
+    Add given number of tabs and return the string
+    """
+    curr_str = "\t"*num_tabs
+    return curr_str
+
+# =============================================================================
 
 def form_in_timing_group(clk_name, timing_type, common_lib_data):
     '''
@@ -331,19 +408,19 @@ def form_in_timing_group(clk_name, timing_type, common_lib_data):
         fall_constraint_val = common_lib_data["input_timing"][
             'hold_rising_fall_constraint_val']
 
-    curr_str = "\n\t\t\ttiming () {{\n\t\t\t\trelated_pin : \"{}\";".format(
+    curr_str = "\n{}timing () {{\n{}related_pin : \"{}\";".format(
+        add_tab(4),
+        add_tab(5),
         clk_name
     )
-    curr_str += "\n\t\t\t\ttiming_type : {};".format(timing_type)
-    curr_str += "\n\t\t\t\trise_constraint (scalar) {{\n\t\t\t\t\tvalues({});".format(
-        rise_constraint_val
+    curr_str += "\n{}timing_type : {};".format(add_tab(5), timing_type)
+    curr_str += "\n{}rise_constraint (scalar) {{\n{}values({});\n{}}}".format(
+        add_tab(5), add_tab(6), rise_constraint_val, add_tab(5)
     )
-    curr_str += "\n\t\t\t\t}"
-    curr_str += "\n\t\t\t\tfall_constraint (scalar) {{\n\t\t\t\t\tvalues({});".format(
-        fall_constraint_val
+    curr_str += "\n{}fall_constraint (scalar) {{\n{}values({});\n{}}}".format(
+        add_tab(5), add_tab(6), fall_constraint_val, add_tab(5)
     )
-    curr_str += "\n\t\t\t\t}"
-    curr_str += "\n\t\t\t}"
+    curr_str += "\n{}}}".format(add_tab(4))
     return curr_str
 
 
@@ -358,8 +435,7 @@ def parse_xml(xml_file):
     xml_tree = ET.parse(xml_file, parser)
     xml_root = xml_tree.getroot()
 
-    port_names = []
-
+    port_names = defaultdict(set)
     # Get the "IO" section
     xml_io = xml_root.find("IO")
     if xml_io is None:
@@ -368,23 +444,19 @@ def parse_xml(xml_file):
 
     xml_top_io = xml_io.find("TOP_IO")
     if xml_top_io is not None:
-        ports = parse_xml_io(xml_top_io)
-        port_names.extend(ports)
+        port_names = parse_xml_io(xml_top_io, port_names)
 
     xml_bottom_io = xml_io.find("BOTTOM_IO")
     if xml_bottom_io is not None:
-        ports = parse_xml_io(xml_bottom_io)
-        port_names.extend(ports)
+        port_names = parse_xml_io(xml_bottom_io, port_names)
 
     xml_left_io = xml_io.find("LEFT_IO")
     if xml_left_io is not None:
-        ports = parse_xml_io(xml_left_io)
-        port_names.extend(ports)
+        port_names = parse_xml_io(xml_left_io, port_names)
 
     xml_right_io = xml_io.find("RIGHT_IO")
     if xml_right_io is not None:
-        ports = parse_xml_io(xml_right_io)
-        port_names.extend(ports)
+        port_names = parse_xml_io(xml_right_io, port_names)
 
     return port_names
 
@@ -392,17 +464,19 @@ def parse_xml(xml_file):
 # =============================================================================
 
 
-def parse_xml_io(xml_io):
+def parse_xml_io(xml_io, port_names):
     """
     Parses xml and get data for mapped_name key
     """
     assert xml_io is not None
-    port_names = []
     for xml_cell in xml_io.findall("CELL"):
         mapped_name = xml_cell.get("mapped_name")
         # define properties for scalar pins
         scalar_mapped_pins = vec_to_scalar(mapped_name)
-        port_names.extend(scalar_mapped_pins)
+        if mapped_name.find('F2A') != -1:
+            port_names['F2A'].update(scalar_mapped_pins)
+        elif mapped_name.find('A2F') != -1:
+            port_names['A2F'].update(scalar_mapped_pins)
     return port_names
 
 
@@ -428,7 +502,7 @@ def vec_to_scalar(port_name):
         lsb = int(bus[:bus.find(':')])
         msb = int(bus[bus.find(':') + 1:])
         if lsb > msb:
-            for i in range(msb, lsb + 1):
+            for i in range(lsb, msb - 1, -1):
                 curr_port_name = port_name[:open_brace] + '[' + str(i) + ']'
                 scalar_ports.append(curr_port_name)
         else:
