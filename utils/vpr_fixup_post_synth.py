65e2375d7fd029b0900332700b5ec83b61354953
Maciej Kurc <mkurc@antmicro.com>
2021-05-10 09:24:32 +0200

    Added mode fixups to the fixup script
    Signed-off-by: Maciej Kurc <mkurc@antmicro.com>

--- utils/vpr_fixup_post_synth.py
+++ utils/vpr_fixup_post_synth.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python3
 """
-A simple script that fixes up post-pnr verilog files from VPR.
+A simple script that fixes up post-pnr verilog files from VPR:
 
-It appends a correct prefix for  each occurrence of a binary string in round
-brackets. For example "(010101)" is converted into "(6'b010101)".
+ - Removes incorrect constants "1'b0" connected to unconnected cell port,
+
+ - Disconnects all unconnected outputs from the "DummyOut" net,
+
+ - appends a correct prefix for  each occurrence of a binary string in round
+   brackets. For example "(010101)" is converted into "(6'b010101)".
 
 One shortcoming of the script is that it may treat a decimal value of 10, 100
 etc. as binary. Fortunately decimal literals haven't been observed to appear
@@ -36,6 +40,32 @@ def main():
     with open(args.i, "r") as fp:
         code = fp.read()
 
+    # Remove connection to 1'b0 from all ports. Do this before fixing up
+    # binary string prefixes so binary parameters won't be affected
+    code = re.sub(
+        r"\.(?P<port>\w+)\s*\(\s*1'b0\s*\)", r".\g<port>(1'bZ)", code
+    )
+
+    # Remove connections to the "DummyOut" net
+    code = re.sub(
+        r"\.(?P<port>\w+)\s*\(\s*DummyOut\s*\)", r".\g<port>()", code
+    )
+
+    # Fixup multi-bit port connections
+    def sub_func_1(match):
+        port = match.group("port")
+        conn = match.group("conn")
+
+        conn = re.sub(r"1'b0", "1'bZ", conn)
+        conn = re.sub(r"DummyOut", "", conn)
+
+        return ".{}({{{}}})".format(port, conn)
+
+    code = re.sub(
+        r"\.(?P<port>\w+)\s*\(\s*{(?P<conn>[^}]*)}\s*\)", sub_func_1, code
+    )
+
+    # Prepend binary literal prefixes
     def sub_func(match):
         assert match is not None
         value = match.group("val")
